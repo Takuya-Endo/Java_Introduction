0829fdbacbc5fbfa54138f489eaf7a1f647ec130
Takuya-Endo <above5purple@gmail.com>
2021-08-07 20:52:03 +0900

    [Update]練習9-1解答

--- Part2/src/exercise_9/Thief.java
+++ Part2/src/exercise_9/Thief.java
@@ -13,15 +13,21 @@ public class Thief {
 	}
 	
 	public Thief(String name, int hp) {
-		this.name = name;
-		this.hp = hp;
-		this.mp = 5;
+		
+		this(name, hp, 5);
+		
+//		this.name = name;
+//		this.hp = hp;
+//		this.mp = 5;
 	}
 	
 	public Thief(String name) {
-		this.name = name;
-		this.hp = 40;
-		this.mp = 5;
+		
+		this(name, 40);
+		
+//		this.name = name;
+//		this.hp = 40;
+//		this.mp = 5;
 	}
 
 }
