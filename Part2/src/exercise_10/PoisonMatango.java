7cff5b4d3902a416994794b39b04a65726ac3c9c
Takuya-Endo <above5purple@gmail.com>
2021-08-08 10:52:38 +0900

    [Update]練習10-3

--- Part2/src/exercise_10/PoisonMatango.java
+++ Part2/src/exercise_10/PoisonMatango.java
@@ -1,7 +1,36 @@
 package exercise_10;
 
-public class PoisonMatango {
+public class PoisonMatango extends Matango {
 	
+//	int hp = 50;
+//	char suffix;	
+	int poisonAttackLimit = 5;
+
+//	public Matango(char suffix) {
+//		this.suffix = suffix;
+//	}
+	public PoisonMatango(char suffix) {
+		super(suffix);
+	}
 	
+	public void attack(Hero h) {
+		System.out.println("キノコ" + this.suffix + "の攻撃");
+		System.out.println("10のダメージ");
+		h.hp -= 10;
+		
+		if (this.poisonAttackLimit > 0) {
+			System.out.println("さらに毒の胞子をばらまいた！");
+			System.out.println(h.hp / 5 + "ポイントのダメージ！");
+			h.hp -= h.hp / 5;
+			this.poisonAttackLimit--;
+		}
+		
+	}
+	
+	public void poisonAttack(Hero h) {
+		System.out.println("毒キノコ" + this.suffix + "の毒攻撃");
+		System.out.println("20のダメージ");
+		h.hp -= 20;
+	}
 
 }
