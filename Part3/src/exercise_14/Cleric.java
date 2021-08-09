87c60900af48d8ea10f502e8198b933245e43f40
Takuya-Endo <above5purple@gmail.com>
2021-08-10 07:00:21 +0900

    [Update]練習14-2

--- Part3/src/exercise_14/Cleric.java
+++ Part3/src/exercise_14/Cleric.java
@@ -4,9 +4,9 @@ public class Cleric {
 	
 	String name = "聖職者「クレリック」";
 	int hp = 50;
-	final int MAX_HP = 50;
+	final static int MAX_HP = 50;
 	int mp = 10;
-	final int MAX_MP = 10;
+	final static int MAX_MP = 10;
 	
 	public void selfAid() {
 		this.mp -= 5;
