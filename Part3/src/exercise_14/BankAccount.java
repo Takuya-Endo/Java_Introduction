b5a375ae200cc31550e617d6bb1564edc53c573a
Takuya-Endo <above5purple@gmail.com>
2021-08-09 22:45:43 +0900

    [Update]練習14-1

--- Part3/src/exercise_14/BankAccount.java
+++ Part3/src/exercise_14/BankAccount.java
@@ -4,5 +4,18 @@ public class BankAccount {
 	
 	private String accountNumber;
 	private int balance;
+	
+	public BankAccount(String accountNumber, int balance) {
+		
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		
+	}
+	
+	public String toString() {
+		
+		return "\\" + this.balance + "（口座番号：" + this.accountNumber + "）";
+		
+	}
 
 }
---
+++ Part3/src/exercise_14/exercise14_1.java
@@ -0,0 +1,12 @@
+package exercise_14;
+
+public class exercise14_1 {
+	
+	public static void main(String[] args) {
+		
+		BankAccount a = new BankAccount("4649", 1592);
+		
+		System.out.println(a);	
+	}
+
+}
