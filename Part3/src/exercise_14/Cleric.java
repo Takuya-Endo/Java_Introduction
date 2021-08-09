e13772d4dfb05005d3789b2358b45473f6df3456
Takuya-Endo <above5purple@gmail.com>
2021-08-10 06:59:35 +0900

    [Add]練習14-2

---
+++ Part3/src/exercise_14/Cleric.java
@@ -0,0 +1,33 @@
+package exercise_14;
+
+public class Cleric {
+	
+	String name = "聖職者「クレリック」";
+	int hp = 50;
+	final int MAX_HP = 50;
+	int mp = 10;
+	final int MAX_MP = 10;
+	
+	public void selfAid() {
+		this.mp -= 5;
+		this.hp = MAX_HP;
+	}
+	
+	public int pray(int praySecond) {
+		
+		int recoveryAmount = praySecond + new java.util.Random().nextInt(3);
+		
+		if ((this.mp + recoveryAmount) <= MAX_MP) {
+			
+			this.mp += recoveryAmount;			
+			return this.mp;
+			
+		} else {
+			
+			return MAX_MP;
+			
+		}
+		
+	}
+
+}
