c839c9830851476b96b9ce7f92f3acc7f3d91ec9
Takuya-Endo <above5purple@gmail.com>
2021-08-10 17:02:09 +0900

    [Update]練習15-1-②

--- Part3/src/exercise_15/exercise_15_1.java
+++ Part3/src/exercise_15/exercise_15_1.java
@@ -5,9 +5,10 @@ public class exercise_15_1 {
 	public static void main(String[] args) {
 		
 		int number =1;
+		int length = 100;
 		StringBuilder stringBuilder = new StringBuilder();
 		
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < length; i++) {
 			
 			stringBuilder.append(number);
 			stringBuilder.append(", ");
@@ -17,6 +18,19 @@ public class exercise_15_1 {
 		
 		String s = stringBuilder.toString();
 		System.out.println(s);
+		
+		String[] a = new String[length];
+		
+		for (int i = 0; i < length; i ++) {
+			
+			a[i] = s.substring(0, s.indexOf(", "));
+			s = s.substring(s.indexOf(", ") + 2);
+		
+		}
+		
+		System.out.println(a[0]);
+		System.out.println(a[50]);
+		System.out.println(a[99]);
 		
 	}
 
