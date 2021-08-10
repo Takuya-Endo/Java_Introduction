43712d4e020b9f9c5bbccba290bba39666dc2066
Takuya-Endo <above5purple@gmail.com>
2021-08-10 15:57:16 +0900

    [Update]練習15-1-①

--- Part3/src/exercise_15/exercise_15_1.java
+++ Part3/src/exercise_15/exercise_15_1.java
@@ -2,6 +2,22 @@ package exercise_15;
 
 public class exercise_15_1 {
 	
-	
+	public static void main(String[] args) {
+		
+		int number =1;
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		for (int i = 0; i < 10; i++) {
+			
+			stringBuilder.append(number);
+			stringBuilder.append(", ");
+			number++;
+			
+		}
+		
+		String s = stringBuilder.toString();
+		System.out.println(s);
+		
+	}
 
 }
