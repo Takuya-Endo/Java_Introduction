04650a32882c6ac9feb48f7f00c7d485a96f6a5a
Takuya-Endo <above5purple@gmail.com>
2021-08-11 10:17:10 +0900

    [Update]練習15-4

--- Part3/src/exercise_15/exercise_15_4.java
+++ Part3/src/exercise_15/exercise_15_4.java
@@ -1,5 +1,28 @@
 package exercise_15;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class exercise_15_4 {
+	
+	public static void main(String[] args) {
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("西暦yyyy年MM月dd日");
+		
+		Date now = new Date();
+		String nowString = dateFormat.format(now);
+		System.out.println("現在の日時は" + nowString);
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(now);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		calendar.set(Calendar.DAY_OF_MONTH, day + 100);
+		Date future = calendar.getTime();
+		
+		String futureString = dateFormat.format(future);
+		System.out.println("１００日後は" + futureString);
+		
+	}
 
 }
