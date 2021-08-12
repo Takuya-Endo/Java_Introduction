df3a20ce76f9c8329a383c5c73520a6f87005e8f
Takuya-Endo <above5purple@gmail.com>
2021-08-12 09:48:59 +0900

    [Update]練習17-2

--- Part3/src/exercise_17/exercise_17_1.java
+++ Part3/src/exercise_17/exercise_17_1.java
@@ -4,8 +4,19 @@ public class exercise_17_1 {
 	
 	public static void main(String[] args) {
 		
-		String s = null;
-		System.out.println(s.length());
+		try {
+			
+			String s = null;
+			System.out.println(s.length());	
+			
+		} catch (NullPointerException e) {
+			
+			System.out.println("NullPointerException例外をcatchしました");
+			System.out.println("――スタックトレース(ここから)――");
+			e.printStackTrace();
+			System.out.println("――スタックトレース(ここまで)――");
+			
+		}
 		
 	}
 
