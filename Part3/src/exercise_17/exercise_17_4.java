4cb7239356b03a04e6201de2032800413b59ae7c
Takuya-Endo <above5purple@gmail.com>
2021-08-12 23:28:18 +0900

    [Update]練習17-4

--- Part3/src/exercise_17/exercise_17_4.java
+++ Part3/src/exercise_17/exercise_17_4.java
@@ -1,10 +1,24 @@
 package exercise_17;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class exercise_17_4 {
 	
 	public static void main(String[] args) {
 		
+		String pathName = "存在しないファイル";
+		File file = new File(pathName);
 		
+		try (FileWriter fileWriter = new FileWriter(file);) {
+			if (pathName.equals("存在しないファイル")) {
+				throw new IOException();
+			}
+			System.out.println("ファイルの出力");
+		} catch (IOException e) {
+			System.out.println("異常終了：指定されたファイルが存在しません");
+		}
 		
 	}
 
