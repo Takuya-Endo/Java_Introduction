b7ae10af9f28cd02e8db201b5bbd930558ea90bb
Takuya-Endo <above5purple@gmail.com>
2021-08-12 22:21:15 +0900

    [Update]練習17-3

--- Part3/src/exercise_17/exercise_17_3.java
+++ Part3/src/exercise_17/exercise_17_3.java
@@ -4,7 +4,14 @@ public class exercise_17_3 {
 	
 	public static void main(String[] args) {
 		
+		String stringIntI = "三";
 		
+		try {
+			int i = Integer.parseInt(stringIntI);
+			System.out.println(i);
+		} catch (NumberFormatException e) {
+			System.out.println("文字列「" + stringIntI + "」は解析可能な整数型ではありません");
+		}
 		
 	}
 
