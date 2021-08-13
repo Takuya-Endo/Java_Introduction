584cf273f8ba5bbd26ecb02eebe1fed586506e91
Takuya-Endo <above5purple@gmail.com>
2021-08-13 15:18:10 +0900

    [Update]練習8-4

--- SecondLap_Part2/src/exercise_8/Cleric.java
+++ SecondLap_Part2/src/exercise_8/Cleric.java
@@ -1,7 +1,33 @@
 package exercise_8;
 
 public class Cleric {
+		
+	private String name;
+	private int hp;
+	private final int MAX_HP = 50;
+	private int mp;
+	private final int MAX_MP = 10;
 	
+	public Cleric() {
+		this.hp = MAX_HP;
+		this.hp = MAX_HP;
+	}
 	
+	public void selfAid() {
+		this.mp -= 5;
+		this.hp = MAX_HP;
+	}
+	
+	public int pray(int second) {
+		int recoveredMp;
+		int point = second + new java.util.Random().nextInt(3);
+		if (this.mp + point <= MAX_MP) {
+			recoveredMp = point;
+		} else {
+			recoveredMp = MAX_MP - this.mp;
+		}
+		return recoveredMp;
+	}
+
 
 }
