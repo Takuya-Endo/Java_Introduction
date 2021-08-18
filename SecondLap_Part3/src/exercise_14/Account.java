ddb63065dff3a90f3b7455473284fb7f364419d9
Takuya-Endo <above5purple@gmail.com>
2021-08-19 05:25:57 +0900

    [Update]練習14-1-②

--- SecondLap_Part3/src/exercise_14/Account.java
+++ SecondLap_Part3/src/exercise_14/Account.java
@@ -13,5 +13,16 @@ public class Account {
 	public String toString() {
 		return "\\" + this.balance + "（口座番号：" +  this.accountNumber + "）";
 	}
+	
+	public boolean equals(Object object) {
+		Account account = (Account) object;
+		if (this.accountNumber == account.accountNumber) {
+			return true;
+		} else if (this.accountNumber.equals(account.accountNumber.trim())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 }
--- SecondLap_Part3/src/exercise_14/Exercise_14_1.java
+++ SecondLap_Part3/src/exercise_14/Exercise_14_1.java
@@ -5,8 +5,24 @@ public class Exercise_14_1 {
 	public static void main(String[] args) {
 		
 		Account a = new Account("4649", 1592);
+		Account b = new Account(" 4649", 1592);
+		Account c = new Account("4649", 0);
 		
 		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		if (a.equals(b)) {
+			System.out.println("aとbは等価である");
+		} else {
+			System.out.println("aとbは等価でない");
+		}
+		
+		if (a.equals(c)) {
+			System.out.println("aとcは等価である");
+		} else {
+			System.out.println("aとcは等価でない");
+		}
 		
 	}
 
