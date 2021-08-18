003057fdad8a0e3631fe52049f753d959272cfa5
Takuya-Endo <above5purple@gmail.com>
2021-08-19 04:46:38 +0900

    [Update]練習14-1

---
+++ SecondLap_Part3/src/exercise_14/Account.java
@@ -0,0 +1,13 @@
+package exercise_14;
+
+public class Account {
+	
+	private String accountNumber;
+	private int balance;
+	
+	public Account(String accountNumber, int balance) {
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+	}
+
+}
