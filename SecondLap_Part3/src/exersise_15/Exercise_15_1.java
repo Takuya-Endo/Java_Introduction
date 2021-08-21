b9c964d53225ab07132cb2107adc503f1f468e17
Takuya-Endo <above5purple@gmail.com>
2021-08-21 09:48:48 +0900

    [Update]練習15-1

--- SecondLap_Part3/src/exersise_15/Exercise_15_1.java
+++ SecondLap_Part3/src/exersise_15/Exercise_15_1.java
@@ -4,14 +4,26 @@ public class Exercise_15_1 {
 	
 	public static void main(String[] args) {
 		
-		int times = 20;
+		int times = 100;
 		StringBuilder stringBuilder = new StringBuilder();
 		
 		for (int i = 1; i < times + 1; i++) {
 			stringBuilder.append(i).append(",");
 		}
 		
-		System.out.println(stringBuilder);	
+//		System.out.println(stringBuilder.toString());
+//		System.out.println(stringBuilder);
+		
+		String number = stringBuilder.toString();
+		
+		String[] numbers = new String[times];
+		numbers = number.split(",");
+		
+		System.out.println(numbers[0]);
+		System.out.println(numbers[1]);
+		System.out.println(numbers[50]);
+		System.out.println(numbers[times - 1]);
+		
 	}
 
 }
